f418ef2276ed25f706c9e46275e5de82b6a8ce74
Raj <kmrrajj@gmail.com>
2018-04-01 23:39:57 +0530

    fixed typo

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,5 +2,5 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
-const buildPath = path.resolve(_dirname, 'build');
+const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
