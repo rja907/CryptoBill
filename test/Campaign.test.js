28ef5e5645ae3582e3397badce676607b2e612f7
Raj <kmrrajj@gmail.com>
2018-04-02 23:45:08 +0530

    decalared initial variables

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -5,3 +5,8 @@ const web3  = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
