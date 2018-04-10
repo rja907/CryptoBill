613b75a064d0f2d10e42519042aabb0119d160ae
Raj <kmrrajj@gmail.com>
2018-04-10 16:15:55 +0530

    modified and deployed contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xAcDc394FF1A1a09374Fcbc62E99D9c9348Bb233c'
+  '0x6F18b6c224747c81258EFB1edE4E4Ff82E69a500'
 );
 
 export default instance;
