d035c69160cf839d3d875cdadcfcb85e4bf1a74b
Raj <kmrrajj@gmail.com>
2018-04-10 14:45:08 +0530

    refactored Layout Head

--- pages/index.js
+++ pages/index.js
@@ -26,17 +26,15 @@ class CampaignIndex extends Component {
     return(
     <Layout>
       <div>
-        <link
-        rel="stylesheet"
-        href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css">
-        </link>
         <h3>Open Campaigns</h3>
-        {this.renderCampaigns()}
         <Button
+          floated="right"
           content="Create Campaign"
           icon="add circle"
           secondary
         />
+
+        {this.renderCampaigns()}
       </div>
     </Layout>
   );
