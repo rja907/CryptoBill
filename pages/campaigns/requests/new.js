68550a9ccc3d5e76ec3c4b3e700cbaef1e450b1e
Raj <kmrrajj@gmail.com>
2018-04-12 10:39:22 +0530

    added error handling a back button to create request

--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -9,7 +9,9 @@ class  RequestNew extends Component {
   state = {
     value: '',
     description: '',
-    recipient: ''
+    recipient: '',
+    loading: false,
+    errorMessage: ''
   };
 
   static async getInitialProps(props) {
@@ -21,24 +23,33 @@ class  RequestNew extends Component {
     event.preventDefault();
     const campaign = Campaign(this.props.address);
     const { description, value, recipient } = this.state;
+    this.setState({ loading: true, errorMessage: '' })
 
     try {
       const accounts = await web3.eth.getAccounts();
       await campaign.methods
         .createRequest(description, web3.utils.toWei(value, 'ether'), recipient)
         .send({ from: accounts[0]});
+      Router.pushRoute(`/campaigns/${this.props.address}/requests`)
     } catch (err) {
-
+      this.setState({ errorMessage: err.message });
     }
+
+    this.setState({ loading: false });
   };
 
   render() {
     return (
       <Layout>
+        <Link route={`/campaigns/${this.props.address}/requests`}>
+          <a>
+            Back
+          </a>
+        </Link>
         <h1>
           Create a Request!
         </h1>
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>
               Description
@@ -69,7 +80,11 @@ class  RequestNew extends Component {
               this.setState({ recipient: event.target.value })}
             />
           </Form.Field>
-          <Button primary>
+          <Message error header="Oops" content={this.state.errorMessage} />
+          <Button
+          primary
+          loading={this.state.loading}
+          >
             Create!
           </Button>
         </Form>
