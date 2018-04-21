9aa6a4ccf95f1ec84082251f1c39426b3e92c925
Raj <kmrrajj@gmail.com>
2018-04-22 00:56:22 +0530

    changed name

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
       <Link route="/">
-        <a className="item">CrowdCoin</a>
+        <a className="item">CryptoCrowd</a>
       </Link>
       <Menu.Menu position="right">
         <Link route="/">
